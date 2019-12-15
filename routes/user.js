d746e4964af16108056d1fc29c69c800269cbad9
Raziel <henrique.sonserina@hotmail.com>
2019-12-15 18:22:30 -0300

    Adicionado Post/Get (User) + Arquivo de testes para endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+
+
+// Create a new user
+router.post('/', async (req, res) => {
+  const user = new User(
+    {
+      name: req.body.name,
+      email: req.body.email
+    }
+  );
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch(e) {
+    res.status(400).json({ message: e.error });
+  }
+
+});
+
 
 module.exports = router;

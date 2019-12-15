410cc398fb181d441c4a46344606caaf7e12dc2c
Raziel <henrique.sonserina@hotmail.com>
2019-12-15 18:36:41 -0300

    Criada função middleware para pesquisa de usuário na DB)

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-
 // Create a new user
 router.post('/', async (req, res) => {
   const user = new User(
@@ -18,8 +17,32 @@ router.post('/', async (req, res) => {
   } catch(e) {
     res.status(400).json({ message: e.error });
   }
-
 });
 
+// List all users
+router.get('/', async (req, res) => {
+  
+  try {
+    const users = await User.find();
+    res.json({ users });
+  } catch(e) {
+    res.status(500).json({ message: e.error });
+  }
+});
+
+// Middleware function to get the user
+async function getUser(req, res, next) {
+  try {
+    const user = User.findById(router.params.id);
+    if (user == null) {
+      return res.status(404).json({ message: 'User not found'});
+    } 
+  } catch(e) {
+    return res.status(500).status({ message: e.message });
+  }
+
+  res.user = user;
+  next();
+}
 
 module.exports = router;
